a9ad62bbb8f96d63489cbf5d72061f6344c714bc
Melvin Chng <melvinnnnnnnnnnnnnnnnnnnnnnnnn@gmail.com>
2017-11-05 22:36:14 -0800

    added neighbor

--- snmp.rb
+++ snmp.rb
@@ -2,47 +2,16 @@ require 'ascii_charts'
 require 'snmp'
 include SNMP
 
-def asus_ac68u_interface_translate
-  puts "Information about interface based on discussion in smallnetbuilder forum and observation. SNMP interface translate may not be 100% right."
-  puts
-  puts "================== Asus RT-AC68U Router ===================="
-  puts "| Interface \t | Description                             |"
-  puts "============================================================"
-  puts "| lo      \t | local loopback (internal to router)     |"
-  puts "| eth0    \t | Router chip: WAN, 2.4G, 5G, LAN         |"
-  puts "| eth1    \t | 2.4GHz Wireless Interface               |"
-  puts "| eth2    \t | 5.0GHz Wireless Interface               |"
-  puts "| vlan1-2 \t | two seperate network (normal and guest) |"
-  puts "| br0     \t | LAN bridge for vlan1, eth1, eth2        |"
-  puts "| tun21   \t | VPN Server / Tunneling                  |"
-  puts "============================================================"
-end
-
-#####################################################################################################################
-# Info                                  # Description                                                               #
-#####################################################################################################################
-# host                                  # host                                                                      #
-# community                             # community string                                                          #
-# interval                              # how frequent data is captured                                             #
-# output                                # print walk data (true/false)                                              #
-# ifTable_columns                       # array of columns that want to walk                                        #
-# position_of_octets                    # position of octet in ifTable_columns array (count from 0 from left)       #
-#                                       # * to disable the checking, use very large number (99999)                  #
-# position_of_ifDescr                   # position of descption in ifTable_columns array (count from 0 from left)   #
-#                                       # * to disable the checking, use very large number (99999)                  #
-# position_of_ipNetToMediaNetAddress    # position of descption in ifTable_columns array (count from 0 from left)   #
-#                                       # * to disable the checking, use very large number (99999)                  #
-#####################################################################################################################
-
-def snmp_walk(host, community, output, ifTable_columns, position_of_octets, position_of_ifDescr, position_of_ipNetToMediaNetAddress)
+def snmp_walk(host, community, output, ifTable_columns, position_of_octets, position_of_ifDescr, position_of_ipNetToMediaNetAddress, position_of_ipNetToMediaIfIndex)
   temp = []                                                               # temp array; to store values of octets
-  interface = []                                                          # interface array; store the name of the interface
-  interface_ip = []
+  interface_name = []                                                     # interface array; store the name of the interface
+  interface_ip = []                                                       # store all interface IP
+  interface = []                                                          # store interface for neighbor
 
   SNMP::Manager.open(:host => host, :community => community) do |manager| # open the connection
     manager.walk(ifTable_columns) do |row|                                # walk each interface and get ifTable_columns data
       row.each_with_index { |vb, index|                                   # begin for loop
-        if output                                                         # if output == true
+        if output == true                                                 # if output == true
           print "\t#{vb.value}"                                           # print each ifTable_columns
         end                                                               # end for loop
 
@@ -51,29 +20,33 @@ def snmp_walk(host, community, output, ifTable_columns, position_of_octets, posi
         end                                                               # end if 
       
         if index == position_of_ifDescr                                   # if current index == position_of_ifDescr
-          interface << vb.value                                           # load the current interface to interface array
+          interface_name << vb.value                                      # load the current interface to interface array
         end                                                               # end if 
 
         if index == position_of_octets                                    # if current index == position_of_octets
           temp << vb.value                                                # load the current octets value to interface array
         end                                                               # end if
+
+        if index == position_of_ipNetToMediaIfIndex                       # if current index == position_of_octets
+          interface << vb.value                                           # load the current octets value to interface array
+        end                                                               # end if
       }
 
-      if output                                                           # if output is enabled
+      if output == true                                                   # if output is enabled
         puts                                                              # print new line
       end                                                                 # end if                                                                
     end                                                                   # end row
   end                                                                     # end walk
 
-  return {:value => temp, :interface => interface, :interface_ip => interface_ip} # put the temp and interface in hash and return
+  return {:value => temp, :interface_name => interface_name, :interface_ip => interface_ip, :interface => interface} # put the temp and interface in hash and return
 end
 
-def get_speed(host, community, interval, output, ifTable_columns, position_of_octets, position_of_ifDescr, position_of_ipNetToMediaNetAddress)
+def get_speed(host, community, interval, output, ifTable_columns, position_of_octets, position_of_ifDescr, position_of_ipNetToMediaNetAddress, position_of_ipNetToMediaIfIndex)
   speed = []                                                              # array of speed to store the calculation of speed
   
-  one = snmp_walk(host, community, output, ifTable_columns, position_of_octets, position_of_ifDescr, position_of_ipNetToMediaNetAddress)  # get hash and assign to one
+  one = snmp_walk(host, community, output, ifTable_columns, position_of_octets, position_of_ifDescr, position_of_ipNetToMediaNetAddress, position_of_ipNetToMediaIfIndex)  # get hash and assign to one
   sleep interval                                                                                      # sleep for interval (second)
-  two = snmp_walk(host, community, output, ifTable_columns, position_of_octets, position_of_ifDescr, position_of_ipNetToMediaNetAddress)  # get hash and assign to two
+  two = snmp_walk(host, community, output, ifTable_columns, position_of_octets, position_of_ifDescr, position_of_ipNetToMediaNetAddress, position_of_ipNetToMediaIfIndex)  # get hash and assign to two
 
   if one.count == two.count      
     one[:value].zip(two[:value]) { |a, b|                                   # load two hash, get values
@@ -88,12 +61,12 @@ def get_speed(host, community, interval, output, ifTable_columns, position_of_oc
   return {:one => one, :two => two, :speed => speed}                      # return one, two, and speed hash
 end
 
-def plot_graph(host, community, interval, output, ifTable_columns, position_of_octets, position_of_ifDescr, position_of_ipNetToMediaNetAddress)
-  interface_with_speed = get_speed(host, community, interval, output, ifTable_columns, position_of_octets, position_of_ifDescr, position_of_ipNetToMediaNetAddress)
+def plot_graph(host, community, interval, output, ifTable_columns, position_of_octets, position_of_ifDescr, position_of_ipNetToMediaNetAddress, position_of_ipNetToMediaIfIndex)
+  interface_with_speed = get_speed(host, community, interval, output, ifTable_columns, position_of_octets, position_of_ifDescr, position_of_ipNetToMediaNetAddress, position_of_ipNetToMediaIfIndex)
 
   graph = []                                                              # array of graph, to get [[x_1, y_1], ... , [x_n, y_n]]
 
-  interface_with_speed[:one][:interface].zip(interface_with_speed[:speed]) { |a, b| #get interface name, and speed
+  interface_with_speed[:one][:interface_name].zip(interface_with_speed[:speed]) { |a, b| #get interface name, and speed
      graph << [a, b]                                                      # put it in the form of [x_n, y_n], assign it to graph
   }
 
@@ -102,14 +75,14 @@ def plot_graph(host, community, interval, output, ifTable_columns, position_of_o
   puts "\t Graph of speed (MB/s) vs interface's traffic with #{interval}s sampling rate"
 end
 
-def plot_graph_interface(host, community, interval, iteration, interface, output, ifTable_columns, position_of_octets, position_of_ifDescr, position_of_ipNetToMediaNetAddress)
+def plot_graph_interface(host, community, interval, iteration, interface, output, ifTable_columns, position_of_octets, position_of_ifDescr, position_of_ipNetToMediaNetAddress, position_of_ipNetToMediaIfIndex)
   
   interface_with_speed = []
   graph = []                                                              # array of graph, to get [[x_1, y_1], ... , [x_n, y_n]]
   i = 0
 
   while i <= iteration  do
-    interface_with_speed[i] = get_speed(host, community, interval, output, ifTable_columns, position_of_octets, position_of_ifDescr, position_of_ipNetToMediaNetAddress)
+    interface_with_speed[i] = get_speed(host, community, interval, output, ifTable_columns, position_of_octets, position_of_ifDescr, position_of_ipNetToMediaNetAddress, position_of_ipNetToMediaIfIndex)
     
       a = i*interval
       graph << [a, interface_with_speed[i][:speed][interface]]
@@ -119,7 +92,7 @@ def plot_graph_interface(host, community, interval, iteration, interface, output
 
   print AsciiCharts::Cartesian.new(graph, :bar => true, :hide_zero => false).draw  # draw graph
   puts
-  puts "\tGraph of speed (MB/s) vs traffic for #{interface_with_speed[0][:one][:interface][interface]} with #{interval}s sampling rate \n\t\t\t\tfor #{iteration} iterations"
+  puts "\tGraph of speed (MB/s) vs traffic for #{interface_with_speed[0][:one][:interface_name][interface]} with #{interval}s sampling rate \n\t\t\t\tfor #{iteration} iterations"
 end
 
 def perform_plot_graph_operation                                          # Plot graph
@@ -127,7 +100,7 @@ def perform_plot_graph_operation                                          # Plot
   community = @community
   columns = ["ifDescr", "ifAdminStatus", "ifHCInOctets", "ifHCOutOctets"] #ifIndex not used as I made my own counter
 
-  plot_graph(host, community, 5, false, columns, 2, 0, 99999)             # Set to 99999 as it is used to print IP interface
+  plot_graph(host, community, 5, false, columns, 2, 0, 99999, 99999)             # Set to 99999 as it is used to print IP interface
 end
 
 def perform_plot_graph_operation_interval                                 # Plot graph
@@ -135,32 +108,77 @@ def perform_plot_graph_operation_interval                                 # Plot
   community = @community
   columns = ["ifDescr", "ifAdminStatus", "ifHCInOctets", "ifHCOutOctets"] #ifIndex not used as I made my own counter
 
-  plot_graph_interface(host, community, 0.5, 10, 3, false, columns, 2, 0, 99999)             # Set to 99999 as it is used to print IP interface
+  plot_graph_interface(host, community, 0.5, 10, 3, false, columns, 2, 0, 99999, 99999)  # Set to 99999 as it is used to print IP interface
+end
+
+def get_system_information
+  host = @host
+  community = @community
+
+  puts "################## SYS INFORMATION BEGIN #####################"
+  puts 
+
+  SNMP::Manager.open(:host => host, :community => community) do |manager|
+    response = manager.get(["sysDescr.0", "sysName.0", "sysLocation.0", "sysContact.0"])
+    response.each_varbind do |vb|
+      puts "#{vb.name.to_s}  #{vb.value.to_s}  #{vb.value.asn1_type}"
+    end
+  end
+
+  puts 
+  puts "################## SYS INFORMATION END #######################"
+  puts
 end
 
 def list_all_interface
   host = @host
   community = @community
-  columns = ["ipNetToMediaNetAddress"]
+  columns = ["ipAdEntAddr"]
 
-  get_result = snmp_walk(host, community, false, columns, 99999, 99999, 0) # Set to 99999 as it is used for graph operation
+  get_result = snmp_walk(host, community, false, columns, 99999, 99999, 0, 99999) # Set to 99999 as it is used for graph operation
 
   i = 0
 
-  puts "Interfaces"
-  puts "########################################"
-  puts "# Number \t # IP                  #"
-  puts "########################################"
-  get_result[:interface_ip].each do |a|                                    # Print row index and interface IP
-    puts "# #{i=i+1} \t \t # #{a} " 
-  end
-  puts "########################################"
+  puts "Interface"
+  puts "#########################################"
+  puts "# Number \t # IP                   #"
+  puts "#########################################"
+  get_result[:interface].zip(get_result[:interface_ip]) { |a, b| 
+    puts "# #{a}  \t \t # #{b} \t#" 
+  }
+  puts "#########################################"
+
 end
+
+def list_all_neighbor
+  host = @host
+  community = @community
+  columns = ["ipNetToMediaNetAddress", "ipNetToMediaIfIndex"]
+
+  get_result = snmp_walk(host, community, false, columns, 99999, 99999, 0, 1) # Set to 99999 as it is used for graph operation
+
+  i = 0
+
+  puts "Neighbor"
+  puts "#########################################"
+  puts "# Interface \t # Neighbor             #"
+  puts "#########################################"
+  get_result[:interface].zip(get_result[:interface_ip]) { |a, b| 
+    puts "# #{a}  \t \t # #{b} \t#" 
+  }
+  puts "#########################################"
+
+end
+
 
 @host = "192.168.1.252"
 @community = "public"
 
+#ipAdEntAddr
+
+# get_system_information
 list_all_interface
+list_all_neighbor
 perform_plot_graph_operation
 perform_plot_graph_operation_interval
 
